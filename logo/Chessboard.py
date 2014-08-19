34bd39e9be30956a4ea556e35def5cfca120f1e9
jordanc44 <clark.jordan@outlook.com>
2014-08-19 12:16:47 +0100

    Running logo/Chessboard.py

--- logo/Chessboard.py
+++ logo/Chessboard.py
@@ -1,9 +1,11 @@
 from tealight.logo import move, turn, color
 
 def square(side):
+  
   for i in range(0, 4):
     move(side)
     turn(90)
- 
-    
-square(50)
+
+for n in range(0, 8):
+   square(50)
+    move(3)
