7aa7af7b28932563e1bd74e2f3e9c71314a0cb01
jordanc44 <clark.jordan@outlook.com>
2014-08-19 12:09:11 +0100

    Running logo/Chessboard.py

--- logo/Chessboard.py
+++ logo/Chessboard.py
@@ -4,6 +4,16 @@ def square(side):
   for i in range(0, 4):
     move(side)
     turn(90)
+  def spiral(size):
+  
+  if size > 300:
+    return
+  
+  move(size)
+  turn(90)
+  spiral(size + 5)
+  
+spiral(0)
    
 
 square(50)
