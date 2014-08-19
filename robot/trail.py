29692d72481e4ac13f15619dcf7d9ca3a90ee6c9
jordanc44 <clark.jordan@outlook.com>
2014-08-19 15:08:11 +0100

    Running robot/trail.py

--- robot/trail.py
+++ robot/trail.py
@@ -9,12 +9,14 @@ def go():
   while touch()=="fruit":
    move()
     
-   if right_side()!="fruit" and left_side()!="fruit" and touch()!="fruit":
-      move()
-  
+   
    if touch()!="fruit":
     turn (1)
     go() 
+    
+    if right_side()!="fruit" and left_side()!="fruit" and touch()!="fruit":
+      move()
+      go()
    
 go()
 
