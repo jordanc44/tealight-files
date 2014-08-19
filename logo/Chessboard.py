2bb5bd3df62480e7d4dfc2d6c5d3b49b4f4ec5c0
jordanc44 <clark.jordan@outlook.com>
2014-08-19 12:10:39 +0100

    Running logo/Chessboard.py

--- logo/Chessboard.py
+++ logo/Chessboard.py
@@ -1,13 +1,10 @@
-from tealight.logo import move, turn, color
+from tealight.logo import move, turn, color, 
 
 def square(side):
   for i in range(0, 4):
     move(side)
     turn(90)
   def spiral(size):
-  
-    return
-
   turn(90)
   spiral(size + 5)
   
