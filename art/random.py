fab29c43dc1e3fdc3c1bb2b5d87220a78cee36c9
jordanc44 <clark.jordan@outlook.com>
2014-08-21 10:56:03 +0100

    Running art/random.py

--- art/random.py
+++ art/random.py
@@ -4,7 +4,7 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 from random import shuffle
 
 x = range(200)
-y = range(400_
+y = range(400)
 
 j = image(x,y,"animals/Ant.png") 
 i = image(x,y,"animals/Fish1.png") 
